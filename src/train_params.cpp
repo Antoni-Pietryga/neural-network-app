a65619db3716477f354e574eabde49e351ec846c
MKOchminski <76217472+MKOchminski@users.noreply.github.com>
2022-04-27 06:54:20 +0200

    Remove old src dir

--- src/train_params.cpp
+++
@@ -1,8 +0,0 @@
-#include "train_params.h"
-TrainParameters::TrainParameters(int64_t input_size_, int64_t hidden_size_, int64_t num_epochs_, double learning_rate_){
-	input_size = input_size_;
-	hidden_size = hidden_size_;
-	num_epochs = num_epochs_;
-	learning_rate = learning_rate_;
-		
-}
